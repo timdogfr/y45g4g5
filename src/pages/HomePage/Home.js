7d2255efee90639531fc7638e32717415c4ceedd
timdogfr <103558704+timdogfr@users.noreply.github.com>
2022-04-22 22:51:41 +0300

    Update Home.js

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -23,7 +23,7 @@ function Home() {
   const [supply , setTotalSupply] = useState(0);
   const [feedback, setFeedback] = useState("");
   const [mintAmount, setMintAmount] = useState(1);
-  const [displayCost, setDisplayCost] = useState(10.00);
+  const [displayCost, setDisplayCost] = useState(10.00 MATIC);
   const [CONFIG, SET_CONFIG] = useState({
     CONTRACT_ADDRESS: "",
     SCAN_LINK: "",
